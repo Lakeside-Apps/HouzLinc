a59f56a50d3cb4b8cdc7aa15cdc25da85f50ce25
Christian Fortini <15091389+christianfo@users.noreply.github.com>
2025-04-21 15:37:32 -0700

    Changed publisher name to lakesideapps

--- UnoApp/Platforms/Android/MsalActivity.Android.cs
+++ UnoApp/Platforms/Android/MsalActivity.Android.cs
@@ -7,11 +7,11 @@ namespace UnoApp.Droid;
 // This class is used by Android to intercept the callback from the browser and call OnActivityResult in the MainActivity.
 // However, the redirect URI configured in the Azure portal is different from what the doc above states.
 // See https://learn.microsoft.com/en-us/azure/developer/mobile-apps/azure-mobile-apps/quickstarts/uno/authentication
-// The redirect URI is of the form "msauth://com.ehouz.houzlinc/..."
+// The redirect URI is of the form "msauth://com.lakesideapps.houzlinc/..."
 [Activity(Exported = true)]
 [IntentFilter(new[] { Intent.ActionView },
    Categories = new[] { Intent.CategoryBrowsable, Intent.CategoryDefault },
-   DataHost = "com.ehouz.houzlinc",
+   DataHost = "com.lakesideapps.houzlinc",
    DataScheme = "msauth")]
 internal class MsalActivity : BrowserTabActivity
 {
