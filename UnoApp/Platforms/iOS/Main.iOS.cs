8744a6225ecbf9bdf525a9242ea78abf5c9ae01a
Christian Fortini <15091389+christianfo@users.noreply.github.com>
2025-09-06 17:07:14 -0700

    Enabled Skia Renderer (#154)
    Enabled UNO feature SkiaRenderer for platforms Desktop (Windows & Mac),
    WASM, Android, iOS.
    This does not change anything for platform WinAppSDK (WinUI3).

--- UnoApp/Platforms/iOS/Main.iOS.cs
+++ UnoApp/Platforms/iOS/Main.iOS.cs
@@ -1,4 +1,5 @@
 using UIKit;
+using Uno.UI.Hosting;
 
 namespace UnoApp.iOS;
 public class EntryPoint
@@ -6,8 +7,9 @@ public class EntryPoint
     // This is the main entry point of the application.
     public static void Main(string[] args)
     {
-        // if you want to use a different Application Delegate class from "AppDelegate"
-        // you can specify it here.
-        UIApplication.Main(args, null, typeof(App));
+        var host = UnoPlatformHostBuilder.Create()
+            .App(() => new App())
+            .UseAppleUIKit()
+            .Build();
     }
 }
